eb252626c011a45265c25a7519320fd63c8c8f09
TopHatProductions115 <TopHatProductions115@gmail.com>
2016-01-06 17:32:41 -0500

    Update Superuser Standalone Installer.bat
    requires adb root permission to perform

--- Superuser Standalone Installer.bat
+++ Superuser Standalone Installer.bat
@@ -1,14 +1,13 @@
+
 adb devices
 adb shell
 echo 'checking for your device...'
-exit
-adb reboot
-adb devices
-adb shell
+adb root
+adb shell mount -o rw,remount /
 echo 'ro.kernel/.qemu=1' > /data/local.prop
-exit
 adb reboot
 adb devices
+adb root
 adb shell mount -o remount,rw /system
 adb push /assets/libraries/su /system/xbin/su
 adb shell chown 0.0 /system/xbin/su
